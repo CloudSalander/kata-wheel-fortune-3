72ed09ae5b3cf5d369e1397e03db15e74e5f6ccc
CloudSalander <ruben.as532@gmail.com>
2025-06-19 10:57:13 +0200

    Just simplified fortunewheel

--- classes/FortuneWheel.php
+++ classes/FortuneWheel.php
@@ -3,15 +3,9 @@
 //TODO: Singleton?
 class FortuneWheel {
     const VALUES = [25,50,100,250,500,'Lose','Bankruptcy'];
-    private int $currentValueIndex;
-
-    public function __construct() {
-        $this->currentValueIndex = 0;
-    }
 
     public function throw(): int | string {
-        $this->currentValueIndex = array_rand(self::VALUES);
-        return self::VALUES[$this->currentValueIndex];
+        return self::VALUES[array_rand(self::VALUES)];
     }
 
 }
