be403dd002377df3db2f86780406a1ce3f459748
CloudSalander <ruben.as532@gmail.com>
2025-06-19 11:35:41 +0200

    What I did in 30 minutes

--- classes/Contest.php
+++ classes/Contest.php
@@ -7,6 +7,7 @@ class Contest {
     const CONTESTANTS_NUMBER = 3;
 
     private FortuneWheel $wheel;
+    private int $currentContestant;
     
     public function __construct(private Panel $panel, private array $contestants){
         $this->turnNumber = 0;
@@ -15,14 +16,16 @@ class Contest {
     
     public function play(): void {
         while(!$this->panel->isSolved()) {
-            $currentContestant = $this->turnNumber % self::CONTESTANTS_NUMBER;
-            echo $this->contestants[$currentContestant]->getName()." say a letter!".PHP_EOL;
+            $this->currentContestant = $this->turnNumber % self::CONTESTANTS_NUMBER;
+            echo $this->contestants[$this->playcurrentContestant]->getName()." say a letter!".PHP_EOL;
             $wheelValue = $this->wheel->throw();
-            var_dump($wheelValue);
-            $currentLetter = $this->contestants[$currentContestant]->sayLetter();
-            echo PHP_EOL;
-            if(!$this->panel->solveLetter($currentLetter)) ++$this->turnNumber;
-            $this->panel->show();
+            if($wheelValue == 'Bankruptcy') 
+            else if($wheelValue == 'Lose')
+            else 
+                $currentLetter = $this->contestants[$this->currentContestant]->sayLetter();
+                echo PHP_EOL;
+                if(!$this->panel->solveLetter($currentLetter)) ++$this->turnNumber;
+                $this->panel->show();
         }
     }
 }
